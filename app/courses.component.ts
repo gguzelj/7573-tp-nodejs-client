4579be18a6c8e33ea12641123e9d69ce96803ccb
lmasello <maselloleandro@gmail.com>
2016-11-03 21:33:46 -0300

    Removing unused files

--- app/courses.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    moduleId: module.id,
-    selector: 'my-app',
-    templateUrl: 'views/subjects/show.html'
-})
-
-export class CoursesComponent { }
